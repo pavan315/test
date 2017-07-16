ad9a837f2731e56f6f7fe307a0c8b01bb4b7562e
pavan315 <pavan.vangaru@gmail.com>
2017-07-15 23:08:13 -0400

    code uncommited

--- Pavan/src/apples.java
+++ Pavan/src/apples.java
@@ -4,7 +4,7 @@ public class apples {
 
 	public static void main(String[] args) {
 		Scanner text = new Scanner(System.in);
-		 int test= 40;
+		 int test= 40; 
 		 
 		 if (test < 20){
 			 System.out.println("true");
@@ -13,12 +13,12 @@ public class apples {
 		 }
 		//System.out.println(text.nextLine());
 		//System.out.println("text");
-	/*	double fnum, snum, answer;
+		double fnum, snum, answer;
 		System.out.println("Enter first num: ");
 		 fnum = text.nextint();
 		 System.out.println("Enter second num: ");
 		 snum = text.nextDouble();
 		 answer = fnum+snum;
-		 System.out.println(answer);*/
+		 System.out.println(answer);
 }
 }
